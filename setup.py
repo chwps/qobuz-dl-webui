5e5aea0b83a926a7fab8e5b82fc82e0cd9272394
unknown <riccardofavullo@gmail.com>
2026-05-05 16:26:58 +0200

    chore: bump version to 2.0.9

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = [
 setup(
     name=pkg_name,
     # 2. VERSION RESET FOR YOUR RELEASE
-    version="2.0.8",  
+    version="2.0.9",  
     # 3. AUTHOR INFO
     author="Riccardo (Sei969)",
     author_email="Sei969@users.noreply.github.com",
