156757f74cf95392bbb1d5060ad4570fac71503c
unknown <riccardofavullo@gmail.com>
2026-05-06 20:42:56 +0200

    Bump version to 2.1.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = [
 setup(
     name=pkg_name,
     # 2. VERSION RESET FOR YOUR RELEASE
-    version="2.1.2",  
+    version="2.1.3",  
     # 3. AUTHOR INFO
     author="Riccardo (Sei969)",
     author_email="Sei969@users.noreply.github.com",
